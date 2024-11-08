a1a10829845e0d780c6768778e23ee58b6e3f36d
ArchitaSarin <160559986+ArchitaSarin@users.noreply.github.com>
2024-11-08 15:52:37 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Team from './components/Team';
@@ -17,6 +19,7 @@ function App() {
         <Route path="/team" element={<Team />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
