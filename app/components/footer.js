449875b98cf87fab8f5054509b88a5a75c8badde
ArchitaSarin <160559986+ArchitaSarin@users.noreply.github.com>
2024-11-08 15:53:43 -0800

    Create footer.js

---
+++ app/components/footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Davis Data Driven Change. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
