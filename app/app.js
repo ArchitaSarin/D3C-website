6c918186b5c8d9582787e9bdfd1c90c6c5aabe4a
ArchitaSarin <160559986+ArchitaSarin@users.noreply.github.com>
2024-11-08 15:52:02 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Team from './components/Team';
+import Projects from './components/Projects';
+import './App.css';
 
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
